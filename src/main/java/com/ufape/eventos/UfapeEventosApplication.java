4f68db08ce5c78a25a630db8ed699b899f922ebf
JuanGustah <juangt360graus@gmail.com>
2023-03-29 20:54:05 -0300

    Primeiro commit

---
+++ src/main/java/com/ufape/eventos/UfapeEventosApplication.java
@@ -0,0 +1,13 @@
+package com.ufape.eventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UfapeEventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UfapeEventosApplication.class, args);
+	}
+
+}
